19b7b872675c685ba3319be9e208e0bf0883651a
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-09-01 00:16:52 +0530

    added job applying routes and completed auth

--- server/router/empRoutes.ts
+++ server/router/empRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import ctrl from '../controllers/userControllers'
+import ctrl from '../controllers/empControllers'
 const app: Router = Router();
 
 
-app.get('/login',ctrl.login)
+app.post('/register',ctrl.register)
+app.post('/login',ctrl.login)
+app.post('/addjob',ctrl.postJobs)
 
 export default app
