43119b627c99bea3a29b1adc20fe6b57f83c9163
Luciferlocas <rohitsomvanshi2003@gmail.com>
2024-09-02 11:21:39 +0530

    working on home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,113 +1,149 @@
 import Image from "next/image";
+import Navtop from "./components/Navtop";
+import { Button, Input } from "@nextui-org/react";
+import { CiSearch, CiLocationOn, CiGlobe } from "react-icons/ci";
+import { RiPencilRuler2Line } from "react-icons/ri";
+import Category from "./components/jobs/Category";
+import Featured from "./components/jobs/Featured";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col gap-12">
+      <header className="handlady">
+        <Navtop />
+        <div className="mt-6 flex justify-center px-12 items-center">
+          <div className="flex gap-4 flex-col">
+            <div className="flex flex-col gap-3 max-w-md">
+              <h1 className="text-6xl font-bold text-zinc-700">
+                Discover more than{" "}
+                <span className="text-[#26A4FF]">5000+ Jobs</span>
+              </h1>
+              <Image src="/underline.svg" alt="lady" width={400} height={500} />
+              <p className="text-black/60">
+                Great platform for the job seeker that searching for new career
+                heights and willing to learn.
+              </p>
+            </div>
+            <div className="p-4 flex gap-2 bg-white shadow-md">
+              <Input
+                startContent={<CiSearch size={24} />}
+                variant="underlined"
+                placeholder="Job title or Keyword"
+              />
+              <Input
+                startContent={<CiLocationOn size={24} />}
+                variant="underlined"
+                placeholder="Location"
+              />
+              <Button className="bg-[#4640DE] text-white font-bold px-16">
+                Search my Job
+              </Button>
+            </div>
+          </div>
+          <Image
+            src="/handlady.png"
+            alt="lady"
+            width={500}
+            height={700}
+            className="lg:flex hidden"
+          />
         </div>
-      </div>
+      </header>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <article className="mx-auto w-10/12 flex flex-col gap-6">
+        <div className="flex justify-between items-end">
+          <h1 className="text-3xl font-bold">
+            Explore by <span className="text-[#26A4FF]">category</span>
+          </h1>
+          <span className="text-[#4650DE] font-semibold text-sm">
+            Show all jobs &#8594;
+          </span>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="flex flex-wrap gap-8">
+          <Category
+            icon={<RiPencilRuler2Line size={40} />}
+            title="Design"
+            num="235 jobs available"
+          />
+          <Category
+            icon={<RiPencilRuler2Line size={40} />}
+            title="Design"
+            num="235 jobs available"
+          />
+          <Category
+            icon={<RiPencilRuler2Line size={40} />}
+            title="Design"
+            num="235 jobs available"
+          />
+          <Category
+            icon={<RiPencilRuler2Line size={40} />}
+            title="Design"
+            num="235 jobs available"
+          />
+          <Category
+            icon={<RiPencilRuler2Line size={40} />}
+            title="Design"
+            num="235 jobs available"
+          />
+        </div>
+      </article>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <article className="mx-auto w-10/12 flex flex-col gap-6">
+        <div className="flex justify-between items-end">
+          <h1 className="text-3xl font-bold">
+            Featured <span className="text-[#26A4FF]">jobs</span>
+          </h1>
+          <span className="text-[#4650DE] font-semibold text-sm">
+            Show all jobs &#8594;
+          </span>
+        </div>
+        <div className="flex flex-wrap gap-8">
+          <Featured
+            description="Revolut is looking for Email Marketing to help team ma ..."
+            place="Revault - Madrid"
+            title="Email marketing"
+            type="Full time"
+            skills={["Design", "Marketing"]}
+            logo={<CiGlobe size={40} />}
+          />
+          <Featured
+            description="Revolut is looking for Email Marketing to help team ma ..."
+            place="Revault - Madrid"
+            title="Email marketing"
+            type="Full time"
+            skills={["Design", "Marketing"]}
+            logo={<CiGlobe size={40} />}
+          />
+          <Featured
+            description="Revolut is looking for Email Marketing to help team ma ..."
+            place="Revault - Madrid"
+            title="Email marketing"
+            type="Full time"
+            skills={["Design", "Marketing"]}
+            logo={<CiGlobe size={40} />}
+          />
+          <Featured
+            description="Revolut is looking for Email Marketing to help team ma ..."
+            place="Revault - Madrid"
+            title="Email marketing"
+            type="Full time"
+            skills={["Design", "Marketing"]}
+            logo={<CiGlobe size={40} />}
+          />
+        </div>
+      </article>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <article className="mx-auto w-10/12 flex flex-col gap-6">
+        <div className="flex justify-between items-end">
+          <h1 className="text-3xl font-bold">
+            Latest <span className="text-[#26A4FF]">jobs open</span>
+          </h1>
+          <span className="text-[#4650DE] font-semibold text-sm">
+            Show all jobs &#8594;
+          </span>
+        </div>
+      </article>
     </main>
   );
 }
